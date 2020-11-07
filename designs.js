7ac58d1fff5adebf30749d7e9c10cb83dbe3f472
Paul Ivanivsky <ivanivsky@gmail.com>
2020-11-07 17:37:17 -0500

    Full Edits

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
+// store sizePicker and pixelCanvas in variables
+const size = document.getElementById('sizePicker');
+const table = document.getElementById('pixelCanvas');
 
-function makeGrid() {
+// create a listener for the sizePicker, and call the makeGrid()
+size.addEventListener('submit', makeGrid);
 
-// Your code goes here!
+// makeGrid() creates the grid
+function makeGrid(event) {
+  table.innerHTML = '';
+  // store the height and weight selected in a couple of variables
+  const height = document.getElementById('inputHeight').value;
+  const width = document.getElementById('inputWidth').value;
 
+  // generate the grid using the height and width parameters
+  for (let i = 0; i < height; i++) {
+    let row = table.insertRow();
+    for (let j = 0; j < width; j++) {
+      let square = row.insertCell();
+
+      // create a listener for the squares
+      square.addEventListener('click', function(event) {
+        // when the square is clicked, change its background color
+        // to the value of that of the colorPicker
+        event.target.style.backgroundColor = document.getElementById('colorPicker').value;
+      });
+    }
+  // needed to make the grid persistent on the page
+  event.preventDefault();
 }
+};
